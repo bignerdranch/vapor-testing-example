350c8008727368f0485537bf771441ec8e94c5b7
Josh Justice <jjustice@bignerdranch.com>
2017-05-17 20:48:54 -0400

    Test list method

--- Tests/AppTests/PostRequestTests.swift
+++ Tests/AppTests/PostRequestTests.swift
@@ -1,6 +1,7 @@
 import HTTP
 import Vapor
 import XCTest
+@testable import App
 
 class PostRequestTests: TestCase {
     let droplet = try! Droplet.testable()
@@ -17,5 +18,25 @@ class PostRequestTests: TestCase {
             .assertStatus(is: .ok)
             .assertJSON("id", passes: { json in json.int != nil })
             .assertJSON("content", equals: testContent)
+    }
+    
+    func testList() throws {
+        try Post(content: "List test 1").save()
+        try Post(content: "List test 2").save()
+        
+        let response = try droplet.testResponse(to: .get, at: "/posts")
+        
+        XCTAssertEqual(response.status, .ok)
+        
+        guard let json = response.json else {
+            XCTFail("Error getting JSON from response \(response)")
+            return
+        }
+        guard let recordArray = json.array else {
+            XCTFail("expected response json to be an array")
+            return
+        }
+        XCTAssertEqual(recordArray.count, 2)
+        XCTAssertEqual(recordArray.first?.object?["content"]?.string, "List test 1")
     }
 }
